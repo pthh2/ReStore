7828f53d52ab6fc28ecdc3d62e893262573347d4
pthh2 <hphamtrinh@hotmail.com>
2023-01-24 23:52:01 -0600

    End section 3

---
+++ client/src/features/catalog/ProductCard.tsx
@@ -0,0 +1,42 @@
+import { Button, Card, Avatar, CardActions, CardContent, CardMedia, Typography } from "@material-ui/core";
+import CardHeader from "@mui/material/CardHeader/CardHeader";
+import { Product } from "../../app/models/product";
+
+interface Props {
+    product: Product;
+}
+export default function ProductCard({ product }: Props) {
+    return (
+        <Card>
+            <CardHeader
+                avatar={
+                    <Avatar sx={{ bgcolor: 'secondary.main' }}>
+                        {product.name.charAt(0).toUpperCase()}
+                    </Avatar>
+                }
+                title={product.name}
+                titleTypographyProps={{
+                    sx: { fontweight: 'bold', color: 'primary.main' }
+                }}
+            />
+            <CardMedia
+                sx={{ height: 140, backgroundSize: 'contain', bgcolor: 'primary.light' }}
+                image={product.pictureUrl}
+                title={product.name}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" color='secondary' >
+                    ${(product.price / 100).toFixed(2)}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    product.band / {product.type}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Add to card</Button>
+                <Button size="small">View</Button>
+            </CardActions>
+        </Card>
+    );
+
+}
---
+++ client/src/features/catalog/ProductList.tsx
@@ -0,0 +1,20 @@
+import { List, Grid } from "@material-ui/core";
+import { Product } from "../../app/models/product";
+import ProductCard from "./ProductCard";
+
+interface Props {
+    products: Product[];
+}
+
+export default function ProductList({ products }: Props) {
+    return (
+        <Grid container spacing={4}>
+            {products.map(product => (
+                <Grid item xs={4} key={product.id}>
+                    <ProductCard product={product} />
+                </Grid>
+
+            ))}
+        </Grid>
+    )
+}
